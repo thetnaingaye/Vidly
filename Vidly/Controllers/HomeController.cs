4d0ce1166cf3700f84d503b9cf6240219a44d4e0
Naing <Naing@DESKTOP-UUNOUS6>
2018-06-16 22:42:46 +0800

    added home page

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Vidly.Controllers
     {
         public ActionResult Index()
         {
-            return Redirect("~/Movies");
+            return View();
         }
 
         public ActionResult About()
